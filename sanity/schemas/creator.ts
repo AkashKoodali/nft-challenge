16bb056e23ce6817fadfe6dc8390991882334efb
Akash Koodali <akashkoodali9@gmail.com>
2023-01-24 10:28:27 +0530

    Minting is Done..

--- sanity/schemas/creator.ts
+++ sanity/schemas/creator.ts
@@ -1,24 +1,25 @@
-export default {
+import {defineField, defineType} from 'sanity'
+export default defineType({
   name: 'creator',
   title: 'Creator',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
-    },
-    {
+    }),
+    defineField( {
       name: 'address',
       title: 'Address',
       type: 'string',
-    },
-    {
+    }),
+    ({
       name: 'bio',
       title: 'Bio',
       type: 'string',
-    },
-    {
+    }),
+    defineField( {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -26,15 +27,16 @@ export default {
         source: 'name',
         maxLength: 96,
       },
-    },
-    {
+    }),
+    defineField ( {
       name: 'image',
       title: 'Image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
+    }),
     
   ],
 }
+)
